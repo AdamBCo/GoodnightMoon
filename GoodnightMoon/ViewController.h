46ae1100210f926eda0df56154ca30ddcf595b37
Adam Cooper <acooper@rallydev.com>
2014-10-09 11:20:48 -0500

    Initial Commit

---
+++ GoodnightMoon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoodnightMoon
+//
+//  Created by Adam Cooper on 10/9/14.
+//  Copyright (c) 2014 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoodnightMoon/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  GoodnightMoon
+//
+//  Created by Adam Cooper on 10/9/14.
+//  Copyright (c) 2014 Adam Cooper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 6;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    return cell;
+}
+
+@end
